8f82db778a02afe862b116b341253d406597916c
hehe <2390836009@qq.com>
2017-09-05 08:48:22 +0800

    加入图片背景

--- app/src/main/java/com/dlmu/liu/coolweather/coolweather/activity/WeatherActivity.java
+++ app/src/main/java/com/dlmu/liu/coolweather/coolweather/activity/WeatherActivity.java
@@ -1,17 +1,21 @@
 package com.dlmu.liu.coolweather.coolweather.activity;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.dlmu.liu.coolweather.coolweather.R;
 import com.dlmu.liu.coolweather.coolweather.gson.Forecast;
 import com.dlmu.liu.coolweather.coolweather.gson.Weather;
@@ -48,9 +52,16 @@ public class WeatherActivity extends AppCompatActivity {
 
     private TextView sportText;
 
+    private ImageView bingPicImg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT>=21){
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_weather);
         //初始化各种控件
         weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
@@ -64,8 +75,15 @@ public class WeatherActivity extends AppCompatActivity {
         comfortText = (TextView) findViewById(R.id.comfort_text);
         carwashText = (TextView) findViewById(R.id.car_wash_text);
         sportText = (TextView) findViewById(R.id.sport_text);
+        bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString = sharedPreferences.getString("weather",null);
+        String bingPic = sharedPreferences.getString("bing_pic",null);
+        if(bingPic!=null){
+            Glide.with(this).load(bingPic).into(bingPicImg);
+        }else {
+            loadBingPic();
+        }
         if(weatherString!=null){
             //有缓存时直接解析天气数据
             Weather weather =Utility.handleWeatherResponse(weatherString);
@@ -77,7 +95,9 @@ public class WeatherActivity extends AppCompatActivity {
             requestWeather(weatherId);
         }
 
+
     }
+
 
     /**
      * 根据天气id请求城市的天气信息
@@ -117,6 +137,7 @@ public class WeatherActivity extends AppCompatActivity {
 
             }
         });
+        loadBingPic();
     }
 
     /**
@@ -157,5 +178,36 @@ public class WeatherActivity extends AppCompatActivity {
         carwashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * 加载图片
+     */
+
+    private void loadBingPic() {
+        String requestBingPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bingPic = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
+                editor.putString("bing_pic",bingPic);
+                editor.apply();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //从网络中加载图片到imageview
+                        Glide.with(WeatherActivity.this).load(bingPic).into(bingPicImg);
+                    }
+                });
+
+            }
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+        });
     }
 }
